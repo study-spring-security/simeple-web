a0fb78f57e2b25aa794d535e79922e3248e2b54c
jung-ho-1231 <parkjung-1231@naver.com>
2022-10-13 21:47:22 +0900

    feat: add MainPageController

---
+++ src/main/java/com/example/chapter06/MainPageController.java
@@ -0,0 +1,24 @@
+package com.example.chapter06;
+
+import com.example.chapter06.product.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class MainPageController {
+
+    private final ProductService productService;
+
+    @GetMapping("/main")
+    public String main(Authentication a, Model model) {
+        model.addAttribute("username", a.getName());
+        model.addAttribute("products", productService.findAll());
+
+        return "main";
+    }
+
+}
