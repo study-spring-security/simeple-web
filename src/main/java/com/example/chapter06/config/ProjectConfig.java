57ac5dd56739154fd8a543bebb53c2a3e7eed030
jung-ho-1231 <parkjung-1231@naver.com>
2022-10-13 17:45:03 +0900

    feat: formLogin 설정

--- src/main/java/com/example/chapter06/config/ProjectConfig.java
+++ src/main/java/com/example/chapter06/config/ProjectConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @RequiredArgsConstructor
@@ -33,6 +34,20 @@ public class ProjectConfig {
         val auth = http.getSharedObject(AuthenticationManagerBuilder.class);
         auth.authenticationProvider(authenticationProvider);
         return auth.build();
+    }
+
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+
+        http.formLogin()
+                .defaultSuccessUrl("/main", true);
+
+
+        http.authorizeRequests()
+                .anyRequest()
+                .authenticated();
+
+        return http.build();
     }
 
 }
