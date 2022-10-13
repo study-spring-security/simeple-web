b31922243b826589bcd4aab6cbc4b63beebdf7d8
jung-ho-1231 <parkjung-1231@naver.com>
2022-10-13 17:29:42 +0900

    feat: AuthenticationManager Bean 추가

--- src/main/java/com/example/chapter06/config/ProjectConfig.java
+++ src/main/java/com/example/chapter06/config/ProjectConfig.java
@@ -1,20 +1,38 @@
 package com.example.chapter06.config;
 
+import com.example.chapter06.config.security.AuthenticationProviderService;
+import lombok.RequiredArgsConstructor;
+import lombok.val;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
 
 @Configuration
+@RequiredArgsConstructor
 public class ProjectConfig {
 
+    private final AuthenticationProviderService authenticationProvider;
+
+
     @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
     @Bean
-    public SCryptPasswordEncoder sCryptPasswordEncoder(){
+    public SCryptPasswordEncoder sCryptPasswordEncoder() {
         return new SCryptPasswordEncoder();
     }
+
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+        val auth = http.getSharedObject(AuthenticationManagerBuilder.class);
+        auth.authenticationProvider(authenticationProvider);
+        return auth.build();
+    }
+
 }
