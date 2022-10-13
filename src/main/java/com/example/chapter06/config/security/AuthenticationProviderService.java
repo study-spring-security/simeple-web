30f57da5f477e23c5757d7cee43dda2c6fedf76d
jung-ho-1231 <parkjung-1231@naver.com>
2022-10-13 17:29:20 +0900

    feat: add AuthenticationProviderService

---
+++ src/main/java/com/example/chapter06/config/security/AuthenticationProviderService.java
@@ -0,0 +1,56 @@
+package com.example.chapter06.config.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationProviderService implements AuthenticationProvider {
+
+
+    private final JpaUserDetailService userDetailService;
+
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final SCryptPasswordEncoder sCryptPasswordEncoder;
+
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String username = authentication.getName();
+        String password = authentication.getCredentials().toString();
+
+        CustomUserDetail user = userDetailService.loadUserByUsername(username);
+
+        switch (user.getUser().getAlgorithm()) {
+            case BCRYPT:
+                return checkPassword(user, password, bCryptPasswordEncoder);
+            case SCRYPT:
+                return checkPassword(user, password, sCryptPasswordEncoder);
+        }
+
+        throw new BadCredentialsException("Bad credentials");
+    }
+
+    private Authentication checkPassword(CustomUserDetail user, String password, PasswordEncoder encoder) {
+        if (encoder.matches(user.getPassword(), password)) {
+            return new UsernamePasswordAuthenticationToken(user, user.getPassword(), user.getAuthorities());
+        }
+
+        throw new BadCredentialsException("Bad credentials");
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication);
+    }
+
+}
