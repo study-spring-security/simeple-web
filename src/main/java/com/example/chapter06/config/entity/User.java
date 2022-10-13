d0ec384c9b99ebd5a4b2abb0f78976effaf8b3cf
jung-ho-1231 <parkjung-1231@naver.com>
2022-10-13 16:05:48 +0900

    feat: add User Entity

---
+++ src/main/java/com/example/chapter06/config/entity/User.java
@@ -0,0 +1,25 @@
+package com.example.chapter06.config.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String username;
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private EncryptionAlgorithm algorithm;
+
+    @OneToMany(mappedBy = "user", fetch = FetchType.EAGER)
+    private List<Authority> authorities;
+}
