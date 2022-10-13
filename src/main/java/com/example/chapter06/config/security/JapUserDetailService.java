a9031feb6558bc3de9d7ff3cad547595a7a080df
jung-ho-1231 <parkjung-1231@naver.com>
2022-10-13 16:54:01 +0900

    feat: add UserDetail, UserService

---
+++ src/main/java/com/example/chapter06/config/security/JapUserDetailService.java
@@ -0,0 +1,33 @@
+package com.example.chapter06.config.security;
+
+import com.example.chapter06.config.entity.User;
+import com.example.chapter06.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.val;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.function.Supplier;
+
+@Service
+@RequiredArgsConstructor
+public class JapUserDetailService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public CustomUserDetail loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        // 예비 인스턴스를 만들기 위한 공급자 선언
+        Supplier<UsernameNotFoundException> s =
+                () -> new UsernameNotFoundException("Problem during authentication!");
+
+        User user = userRepository
+                .findByUsername(username) // 사용자를 포함한 Optional 인스턴스를 반환하거나 사용자가 없으면 비어 있는 Optional 인스턴스를 반환
+                .orElseThrow(s); // Optional 인스턴스가 비어 있으면 정의된 공급자에서 생성한 예외를 투척하고, 그렇지 않으면 User 인스턴스르 반환
+
+        return new CustomUserDetail(user); // User 인스턴스를 CustomUserDetails 데토레이터로 래핑하고 반환
+    }
+}
